4d3e45e231e1ae54a0302bedd82cff53a34d73ab
Dagim01 <Daginaruto@gmail.com>
2023-03-24 15:48:02 +0300

    create testing tutorial

---
+++ Testing Tuorial/arthimetic_operations.py
@@ -0,0 +1,17 @@
+class ArthimeticClass:
+    def add(x, y):
+        # adding two numbers
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both inputs should be numbers")
+        return x + y
+    def subtract(x, y):
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both inputs should be numbers")
+        return x - y
+        # subtracting two numbers
+    def multiply(x, y):
+        # multiplying two numbers
+        return x * y
+    def divide(x, y):
+        # dividing two numbers
+        return x / y

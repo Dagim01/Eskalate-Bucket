43514fbe4b41537fc9238fe7cbd7d73ae2405d12
Dagmawi Yesuf Mohammed <daginaruto@gmail.com>
2023-03-26 09:17:35 +0300

    testing exercise

--- Testing Tuorial/arthimetic_operations.py
+++ Testing Tuorial/arthimetic_operations.py
@@ -11,7 +11,13 @@ class ArthimeticClass:
         # subtracting two numbers
     def multiply(x, y):
         # multiplying two numbers
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both inputs should be numbers")
         return x * y
     def divide(x, y):
         # dividing two numbers
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both inputs should be numbers")
+        if y==0:
+            raise ZeroDivisionError("Denominator should be different from 0")
         return x / y
--- Testing Tuorial/test_arthimetic_operations.py
+++ Testing Tuorial/test_arthimetic_operations.py
@@ -23,4 +23,42 @@ class TestArithmeticClass:
         #act
         actual_output= ArthimeticClass.subtract(x,y)
         #assert
+        with pytest.raises(TypeError):
+            ArthimeticClass.subtract("3", 2)
+        with pytest.raises(TypeError):
+            ArthimeticClass.subtract(3, "2")
+        with pytest.raises(TypeError):
+            ArthimeticClass.subtract("3", "2")
+        assert expected_output == actual_output
+    def test_multiply(self):
+        #arrange
+        x, y = 3, 2
+        expected_output = 6
+        #act
+        actual_output= ArthimeticClass.multiply(x,y)
+        #assert
+        with pytest.raises(TypeError):
+            ArthimeticClass.multiply("3", 2)
+        with pytest.raises(TypeError):
+            ArthimeticClass.multiply(3, "2")
+        with pytest.raises(TypeError):
+            ArthimeticClass.multiply("3", "2")
+        assert expected_output == actual_output
+
+    def test_divide(self):
+        #arrange
+        x, y = 4, 0
+        expected_output = 2
+        #act
+        actual_output= ArthimeticClass.divide(x,y)
+        #assert
+        with pytest.raises(TypeError):
+            ArthimeticClass.divide("4", 2)
+        with pytest.raises(TypeError):
+            ArthimeticClass.divide(4, "2")
+        with pytest.raises(TypeError):
+            ArthimeticClass.divide("4", "2")
+        with pytest.raises(ZeroDivisionError):
+            ArthimeticClass.divide(4, 0)
+        assert expected_output == actual_output
         
